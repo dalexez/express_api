241aebc8c8c87a70f96827367d46cada9c8c660d
dalexez <dalexez@gmail.com>
2022-04-26 15:34:41 -0500

    Metodo POST

--- app.js
+++ app.js
@@ -27,6 +27,12 @@ app.get('/v1/explorers/:id', (req, res) => {
     res.status(200).json(explorer)
 })
 
+app.post('/v1/explorers', (req, res) => {
+    console.log(`Api Explorers POST request ${new Date()}`)
+    const requestBody = req.body //Parametros de un cliente
+    res.status(201).json({message: "Created"})
+})
+
 //Con essto inicializamos esta app
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
